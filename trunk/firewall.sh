ca4a6ee02598f4f9a643f4683b344dcaf12f1517
idovitz <idovitz@31f05464-7b56-11de-a1b3-85d0aeae5c5b>
2010-01-14 15:17:46 +0000

    - firewall script

---
+++ trunk/firewall.sh
@@ -0,0 +1,30 @@
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD DROP
+
+# INPUT
+iptables -A INPUT -s 127.0.0.1 -d 127.0.0.1 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type 8 -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p udp --dport 1194 -j ACCEPT
+iptables -A INPUT -j LOG --log-prefix "REJECT: "
+iptables -A INPUT -j REJECT
+
+# FORWARD
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# MASQ
+#modprobe ipt_MASQUERADE # If this fails, try continuing anyway
+
+#iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+echo 1 > /proc/sys/net/ipv4/ip_forward
